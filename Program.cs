2e165f8b9875031892c1cef5a56cccc20d5874c5
Suraj Jayaram <SJayaram@dealersocket.com>
2020-03-24 12:18:27 +0530

    Client App for the Auth API

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Program
+    {
+        private static async Task Main()
+        {
+            var client = new HttpClient();
+            
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = "https://sso.local.dealersocket.com/connect/token",
+                ClientId = "crm_campaign_api",
+                ClientSecret = "CrmCampaigApiSecret",
+
+                Scope = "crm_api"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var apiClient = new HttpClient();
+            //apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("http://localhost:5000/Example");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
