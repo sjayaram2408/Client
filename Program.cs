d5fd261796f3ae543ae81aead495ea2730ca85ca
Suraj Jayaram <SJayaram@dealersocket.com>
2020-03-30 20:30:57 +0530

    Chnage made in Program.cs to call the Facebook Create Audience endpoint

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿using IdentityModel.Client;
+﻿using DealerSocket.Crm.Integrations.FacebookAudience.Models;
+using IdentityModel.Client;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Client
@@ -11,7 +14,7 @@ namespace Client
         private static async Task Main()
         {
             //var client = new HttpClient();
-            
+
             //// request token
             //var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
             //{
@@ -27,12 +30,26 @@ namespace Client
 
             //Console.WriteLine(tokenResponse.Json);
             //Console.WriteLine("\n\n");
-
+            AudienceManager facebookAudienceManager = new AudienceManager
+            {
+                Account = new Account
+                {
+                    Id = "exampleAdAccountId",
+                    AccessToken = "exampleAccessToken",
+                    CustomAudienceId = "exampleCustomAudienceId"
+                },
+                AddressId = 1,
+                Description = "exampleDescription",
+                Name = "exampleName",
+                SiteId = 3,
+                UseMock = true
+            };
+            
             // call api
             var apiClient = new HttpClient();
-            apiClient.SetBearerToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjVFMDJSYnBLZHhEZ0tWczlMSVhRN3lQcXFhOCIsImtpZCI6IjVFMDJSYnBLZHhEZ0tWczlMSVhRN3lQcXFhOCJ9.eyJpc3MiOiJodHRwczovL2xvY2FsLmRlYWxlcnNvY2tldC5jb20iLCJhdWQiOiJodHRwczovL2xvY2FsLmRlYWxlcnNvY2tldC5jb20vcmVzb3VyY2VzIiwiZXhwIjoxNTg1MjEwODI1LCJuYmYiOjE1ODUyMDM2MjUsImNsaWVudF9pZCI6ImNybV9ibGFja2JpcmQiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwiZW1haWwiLCJibGFja2JpcmQiLCJzZWFyY2giLCJhcGkiXSwic3ViIjoiMTAwMDk3IiwiYXV0aF90aW1lIjoxNTg1MjAzNjIzLCJpZHAiOiJpZHNydiIsImNybV9yb2xlIjoiYWRtaW4iLCJhZG1pbl9yb2xlIjoic3NvIiwiY3JtX3VzZXJuYW1lIjoiZnNhZmFpciIsImNybV9zdWIiOiJmc2FmYWlyIiwicHJvZHVjdCI6ImNybSIsImFtciI6WyJwYXNzd29yZCJdfQ.ntV8KhWFPCgoXLNprID5cJXqcig00h1z-H3O16zBtzYedk3BUV2qoSoHOs4L_bw4bosTGxtz1V2BiXxIChJOhU1dOCfSQm_5ca-BbsBBKdFXjAXP2JUzX80d-078MIsbJtnq8ZK-hWjRRid0bzmMMsskWKBBSDY54MVHWLl73VTs3Sff_jAs2WrGmsLXyXviPK64nrCvLxjBQkVHj48RY8FOcYXvcmvVmn6kth1BoBkNgHQ1_zBTc05DfVnv0RZ3J2yZJALaf6CW07u90Psb3-J2H0-WiuSk3CAzfVttbm11xWTx2DXyaVrnRzp92T3aV_Z3TgjheZeOpnWcNA1-Mw");
-
-            var response = await apiClient.GetAsync("https://localhost:5001/Identity");
+            apiClient.SetBearerToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjVFMDJSYnBLZHhEZ0tWczlMSVhRN3lQcXFhOCIsImtpZCI6IjVFMDJSYnBLZHhEZ0tWczlMSVhRN3lQcXFhOCJ9.eyJpc3MiOiJodHRwczovL2xvY2FsLmRlYWxlcnNvY2tldC5jb20iLCJhdWQiOiJodHRwczovL2xvY2FsLmRlYWxlcnNvY2tldC5jb20vcmVzb3VyY2VzIiwiZXhwIjoxNTg1NTgwNzczLCJuYmYiOjE1ODU1NzM1NzMsImNsaWVudF9pZCI6ImNybV9ibGFja2JpcmQiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwiZW1haWwiLCJibGFja2JpcmQiLCJzZWFyY2giLCJhcGkiXSwic3ViIjoiMTAwMDk3IiwiYXV0aF90aW1lIjoxNTg1NTY2MzgyLCJpZHAiOiJpZHNydiIsImNybV9yb2xlIjoiYWRtaW4iLCJhZG1pbl9yb2xlIjoic3NvIiwiY3JtX3VzZXJuYW1lIjoiZnNhZmFpciIsImNybV9zdWIiOiJmc2FmYWlyIiwicHJvZHVjdCI6ImNybSIsImFtciI6WyJwYXNzd29yZCJdfQ.LWSvQNn7E2Bk37xJmLhq7-OrAznK12t3WAopnsQjpdHB2hbFiwI5qzftuXQPlCQ1BzDz6Z7y2lGs5c7Bv7D_SjdP8dIJmkOISbrJjAtbDQafvGuKXf0d-hBZAHCnckUD42OTL4piNo5JF4Hz-Y7brE9B3_axVHwmogundH20ZqTaRIvQg60f4y0KRBj3ZT8GGpBg7D95kTf1rJ0d7DBAk64nNwgJz1G--xrG2E8uky9-HeGIDrN7twnrEDNAJeQpmixr6xfb8uTNFI11PFN51GKERM9A1priINVeOOzcuGeXOq9em3g6PMpJ4NU9UEmFMNw12GstZqn-Npak5SguHQ");
+            var stringContent = new StringContent(JsonConvert.SerializeObject(facebookAudienceManager), Encoding.UTF8, "application/json");
+            var response = await apiClient.PostAsync("https://localhost:5001/Facebook", stringContent);
             if (!response.IsSuccessStatusCode)
             {
                 Console.WriteLine(response.StatusCode);
@@ -40,7 +57,7 @@ namespace Client
             else
             {
                 var content = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(JArray.Parse(content));
+                //Console.WriteLine(JArray.Parse(content));
             }
         }
     }
